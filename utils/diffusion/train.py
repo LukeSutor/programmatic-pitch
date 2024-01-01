d157dfc0e1506dbfd33ed96ba479fa976b377772
Luke Sutor <lukesutor@gmail.com>
2024-01-01 18:37:22 -0500

    update ema model in training script

--- utils/diffusion/train.py
+++ utils/diffusion/train.py
@@ -6,23 +6,13 @@ from torch.optim import Adam
 from accelerate import Accelerator
 from tqdm import tqdm
 import itertools
-from .writer import MyWriter
+from ema_pytorch import EMA
 
+from .writer import MyWriter
 from models.diffusion.denoising_diffusion import Unet, GaussianDiffusion
-from models.diffusion.EMA import EMA
 from utils.dataloader import create_dataloader
 from utils.diffusion.validation import validate
 import constants
-
-
-def reset_parameters(ema_model, model):
-    ema_model.load_state_dict(model.state_dict())
-
-def step_ema(step, ema, ema_model, model):
-    if step < constants.STEP_START_EMA:
-        reset_parameters(ema_model, model)
-        return
-    ema.update_model_average(ema_model, model)
 
 
 def train():
@@ -49,18 +39,12 @@ def train():
         sampling_timesteps=4
     ).to(device)
 
-    ema = EMA(constants.EMA_DECAY)
-
-    ema_model = copy.deepcopy(diffusion)
-
     trainloader = create_dataloader(True, device='cpu')
     valloader = create_dataloader(False, device='cpu')
 
     opt = Adam(diffusion.parameters(), lr = constants.DIFFUSION_LR)
                
     diffusion, opt, trainloader = accelerator.prepare(diffusion, opt, trainloader)
-
-    reset_parameters(ema_model, diffusion)
 
     # Create directories and writer
     log_dir = os.path.join(constants.LOG_DIR, constants.RUN_NAME, 'diffusion')
@@ -72,6 +56,8 @@ def train():
         os.makedirs(chkpt_dir, exist_ok=True)
         os.makedirs(results_dir, exist_ok=True)
         writer = MyWriter(log_dir)
+        ema = EMA(diffusion, beta=constants.EMA_DECAY, update_after_step=constants.STEP_START_EMA, update_every=constants.UPDATE_EMA_EVERY).to(device)
+
 
     if constants.DIFFUSION_CHECKPOINT_PATH is not None:
         accelerator.print("Resuming from checkpoint: %s" % constants.DIFFUSION_CHECKPOINT_PATH)
@@ -79,7 +65,8 @@ def train():
         init_epoch = checkpoint['epoch']
         step = checkpoint['step']
         diffusion.load_state_dict(checkpoint['model'])
-        ema_model.load_state_dict(checkpoint['ema'])
+        if accelerator.is_local_main_process:
+            ema.load_state_dict(checkpoint['ema'])
         if accelerator.scaler is not None and checkpoint['scaler'] is not None:
             accelerator.scaler.load_state_dict(checkpoint['scaler'])
 
@@ -118,16 +105,15 @@ def train():
                 writer.log_training(loss.item(), step)
 
             accelerator.wait_for_everyone()
-            # accelerator.clip_grad_norm_(diffusion.parameters(), 1.0)
 
             if step != 0 and step % constants.GRADIENT_ACCUMULATION == 0:
                 opt.step()
                 opt.zero_grad()
-
-            if step % constants.UPDATE_EMA_EVERY == 0:
-                step_ema(step, ema, ema_model, diffusion)
             
             step += 1
+
+            if accelerator.is_local_main_process:
+                ema.update()
 
         if accelerator.is_local_main_process:
             # Log epoch loss
@@ -137,16 +123,16 @@ def train():
             # Validate model
             if epoch != 0 and epoch % constants.VALIDATION_INTERVAL == 0:
                 with torch.no_grad():
-                    validate(ema_model, valloader, writer, step, device)
+                    validate(ema, valloader, writer, step, device)
 
             # Sample model
             if epoch != 0 and epoch % constants.SAMPLE_INTERVAL == 0:
-                ema_model.eval()
+                ema.ema_model.eval()
 
                 milestone = epoch // constants.SAMPLE_INTERVAL
 
                 for i in range(constants.SAMPLE_NUMBER):
-                        image = ema_model.sample(batch_size=1)
+                        image = ema.ema_model.sample(batch_size=1)
                         if i == 0:
                             os.makedirs(os.path.join(results_dir, str(milestone)), exist_ok=True)
                             writer.log_mel_spec(image.squeeze(0).squeeze(0).cpu().detach().numpy(), step)      
@@ -159,7 +145,7 @@ def train():
                     'epoch': epoch,
                     'step': step,
                     'model': diffusion.state_dict(),
-                    'ema': ema_model.state_dict(),
+                    'ema': ema.state_dict(),
                     'scaler': accelerator.scaler.state_dict() if accelerator.scaler is not None else None,
                     'hyperparams': hyperparams,
                 }, save_path)
